d1b0ba7d9b52b02ef94e3273afe5ca54379ee4fa
Devmadia <agathaphan@icloud.com>
2020-10-21 01:09:29 -0500

    contact hooks and tests

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,19 +1,44 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
 
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
-    // JSX
+    const [errorMessage, setErrorMessage] = useState('');
 
     function handleChange(e) {
         setFormState({...formState, name: e.target.value })
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                if (!e.target.value.length) {
+                  setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                  setErrorMessage('');
+                }
+            }
+        }
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+    }
+
+    console.log('errorMessage', errorMessage);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
     }
 
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
                     <input type="text" defaultValue={name} name="name" />
@@ -26,6 +51,11 @@ function ContactForm() {
                     <label htmlFor="message">Message:</label>
                     <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
                 </div>
+                {errorMessage && (
+                <div>
+                    <p className="error-text">{errorMessage}</p>
+                </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -5,14 +5,19 @@ import Nav from '..';
 
 const categories = [
     { name: 'portraits', description: 'Portraits of people in my life' }
-  ]
+]
+
   const mockCurrentCategory = jest.fn();
   const mockSetCurrentCategory = jest.fn();
+  const mockContactSelected = jest.fn();
+  const mockSetContactSelected = jest.fn();
 
-  it('renders', () => {
+it('renders', () => {
     render(<Nav
       categories={categories}
       setCurrentCategory={mockSetCurrentCategory}
       currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
     />);
 })
